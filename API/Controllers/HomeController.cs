6f2fc5c53bd2f3cd0e699c67fb5441066546170f
Abubakr Mahdi <abubakrmahdi20@gmail.com>
2020-03-29 22:58:06 +0200

    Wired Profile and Edit forms.

--- API/Controllers/HomeController.cs
+++ API/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using SudaneseProgComLibrary.Models;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace API.Controllers
@@ -10,6 +11,46 @@ namespace API.Controllers
             ViewBag.Title = "Home Page";
             var Members = new MembersController().Get().ToList();
             return View(Members);
+        }
+
+        [Route("members/profile/{id}")]
+        [HttpGet]
+        public ActionResult Profile(int Id)
+        {
+            ViewBag.Title = "Member Profile";
+            var Member = new MembersController().LoadMemberProfile(Id);
+            return View(Member);
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            ViewBag.Title = "Edit Member";
+            var Member = new MembersController().LoadMemberProfile(Id);
+
+            if (Member.Id == 0)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(Member);
+            }
+        }
+
+        [Route("members/save")]
+        [HttpPost]
+        public ActionResult Save(Member member)
+        {
+            var memControler = new MembersController();
+            if (member.Id == 0)
+            {
+                memControler.Post(member);
+            }
+            else
+            {
+                memControler.Put(member.Id, member);
+            }
+            return RedirectToAction("Index", memControler.Get().ToList());
         }
     }
 }
