83c4c4c865bb70716c680ae55c8629d9508a0866
Abubakr Mahdi <abubakrmahdi20@gmail.com>
2020-03-29 22:56:22 +0200

    Changed the Theme.

--- API/App_Start/BundleConfig.cs
+++ API/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace API
 {
@@ -20,7 +19,7 @@ namespace API
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-slate.css",
                       "~/Content/site.css"));
         }
     }
