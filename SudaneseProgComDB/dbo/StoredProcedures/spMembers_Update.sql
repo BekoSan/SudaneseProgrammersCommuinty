6b5b82aa78dbf52cc8e9a9e9c978c20518bc295a
Abubakr Mahdi <abubakrmahdi20@gmail.com>
2020-03-29 06:53:07 +0200

    Added Update Stored Procedure.
    Stored Procedure to update mebers data.

---
+++ SudaneseProgComDB/dbo/StoredProcedures/spMembers_Update.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[spMembers_Update]
+@FullName nvarchar(250),
+@Job nvarchar(250),
+@Id int 
+AS
+BEGIN
+Update Members Set FullName = @FullName ,Job = @Job  Where Id = @Id
+END
