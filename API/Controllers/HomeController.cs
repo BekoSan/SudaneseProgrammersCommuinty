7198e1d855819113709bda03207db924eda6d076
Abubakr Mahdi <abubakrmahdi20@gmail.com>
2020-04-05 18:50:37 +0200

    Simple Api Home Page.

--- API/Controllers/HomeController.cs
+++ API/Controllers/HomeController.cs
@@ -1,6 +1,4 @@
-﻿using SudaneseProgComLibrary.Models;
-using System.Linq;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace API.Controllers
 {
@@ -9,64 +7,64 @@ namespace API.Controllers
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
-            var Members = new MembersController().Get().ToList();
-            return View(Members);
+            //var Members = new MembersController().Get().ToList();
+            return Content("API Home Page ");
         }
 
-        [Route("members/profile/{id}")]
-        [HttpGet]
-        public ActionResult Profile(int Id)
-        {
-            ViewBag.Title = "Member Profile";
-            var Member = new MembersController().LoadMemberProfile(Id);
-            return View(Member);
-        }
+        //[Route("members/profile/{id}")]
+        //[HttpGet]
+        //public ActionResult Profile(int Id)
+        //{
+        //    ViewBag.Title = "Member Profile";
+        //    var Member = new MembersController().LoadMemberProfile(Id);
+        //    return View(Member);
+        //}
 
-        public ActionResult Edit(int Id)
-        {
-            ViewBag.Title = "Edit Member";
-            var Member = new MembersController().LoadMemberProfile(Id);
+        //public ActionResult Edit(int Id)
+        //{
+        //    ViewBag.Title = "Edit Member";
+        //    var Member = new MembersController().LoadMemberProfile(Id);
 
-            if (Member.Id == 0)
-            {
-                return HttpNotFound();
-            }
-            else
-            {
-                return View("MemberForm", Member);
-            }
-        }
+        //    if (Member.Id == 0)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    else
+        //    {
+        //        return View("MemberForm", Member);
+        //    }
+        //}
 
-        public ActionResult New()
-        {
-            ViewBag.Title = "New Member";
-            return View("MemberForm");
-        }
+        //public ActionResult New()
+        //{
+        //    ViewBag.Title = "New Member";
+        //    return View("MemberForm");
+        //}
 
-        public ActionResult Delete(bool accept, int Id)
-        {
-            var memControler = new MembersController();
-            if (accept)
-            {
-                memControler.Delete(Id);
-            }
-            return RedirectToAction("Index", memControler.Get().ToList());
-        }
+        //public ActionResult Delete(bool accept, int Id)
+        //{
+        //    var memControler = new MembersController();
+        //    if (accept)
+        //    {
+        //        memControler.Delete(Id);
+        //    }
+        //    return RedirectToAction("Index", memControler.Get().ToList());
+        //}
 
-        [Route("members/save")]
-        [HttpPost]
-        public ActionResult Save(Member member)
-        {
-            var memControler = new MembersController();
-            if (member.Id == 0)
-            {
-                memControler.Post(member);
-            }
-            else
-            {
-                memControler.Put(member.Id, member);
-            }
-            return RedirectToAction("Index", memControler.Get().ToList());
-        }
+        //[Route("members/save")]
+        //[HttpPost]
+        //public ActionResult Save(Member member)
+        //{
+        //    var memControler = new MembersController();
+        //    if (member.Id == 0)
+        //    {
+        //        memControler.Post(member);
+        //    }
+        //    else
+        //    {
+        //        memControler.Put(member.Id, member);
+        //    }
+        //    return RedirectToAction("Index", memControler.Get().ToList());
+        //}
     }
 }
