7fd606cfd72777f43a25028b786f73ce05ea0077
Abubakr Mahdi <abubakrmahdi20@gmail.com>
2020-04-05 18:58:04 +0200

    Added ApiHelper class.
    Used to configure the api that  connected with library.

---
+++ SudaneseProgComLibrary/DataAccess/ApiHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace SudaneseProgComLibrary
+{
+    public static class ApiHelper
+    {
+        public static HttpClient ApiClient { get; set; }
+
+        public static string apiUrl = "http://localhost:52891/api";
+
+        public static void InitializeClient()
+        {
+            ApiClient = new HttpClient();
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
